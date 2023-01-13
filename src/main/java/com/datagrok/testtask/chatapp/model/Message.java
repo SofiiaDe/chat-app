ae22c7745fba996bdfcfed6ccf445133cfe62e47
SofiiaDe <sofiia.prydatko@gmail.com>
2023-01-13 21:19:03 +0200

    Add simple POJOs

---
+++ src/main/java/com/datagrok/testtask/chatapp/model/Message.java
@@ -0,0 +1,9 @@
+package com.datagrok.testtask.chatapp.model;
+
+import lombok.*;
+
+@Data
+public class Message {
+
+    private String content;
+}
---
+++ src/main/java/com/datagrok/testtask/chatapp/model/User.java
@@ -0,0 +1,9 @@
+package com.datagrok.testtask.chatapp.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+}
